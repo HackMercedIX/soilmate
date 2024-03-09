d4893dc8b86a1b80e34659386140c0066e34c21d
FlyinPandaa <mikefang456@gmail.com>
2024-03-09 13:51:32 -0800

    Added components folder and header file

---
+++ src/components/header/header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const header = () => {
+    return (
+      <header>
+        <div className="container header__container">
+          <h1> SoilMate </h1>
+          
+        </div>
+      </header>
+    )
+  }
+  
+  export default header
